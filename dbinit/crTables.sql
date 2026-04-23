14538b96a22b94c1a7b858eb8a8c8b80af03b587
YILMAZ SÖNMEZ <ysonmez824@gmail.com>
2026-04-23 19:11:13 +0300

    There are still some features missing in database.

--- dbinit/crTables.sql
+++ dbinit/crTables.sql
@@ -1,3 +1,4 @@
+-- don't use, not ready
 CREATE TABLE department(
 	dept_id INT PRIMARY KEY AUTO_INCREMENT,
 	dept_name varchar(20) NOT NULL UNIQUE,
@@ -9,8 +10,8 @@ CREATE TABLE doctor(
 	first_name varchar(10) NOT NULL,
 	last_name varchar(10) NOT NULL,
 	specialization varchar(20) NOT NULL,
-	phone_num char(11) NOT NULL,
-	email varchar(15),
+	phone_num char(11) UNIQUE NOT NULL,
+	email varchar(15) UNIQUE,
 	doc_gender char(1) CHECK (doc_gender = 'M' OR doc_gender = 'F'),
 	dept_id INT NULL,
 	CONSTRAINT fk_docTdept FOREIGN KEY (dept_id)
@@ -34,8 +35,41 @@ CREATE TABLE patient(
 	patient_id INT PRIMARY KEY AUTO_INCREMENT,
 	first_name varchar(10) NOT NULL,
 	last_name varchar(10) NOT NULL,
-	tc_no char(11) NOT NULL CHECK(CHAR_LENGTH(tc_no) = 11),
+	tc_no char(11) UNIQUE NOT NULL CHECK(CHAR_LENGTH(tc_no) = 11),
 	birth_date DATE,
 	pat_gender char(1) CHECK(pat_gender = 'M' OR pat_gender = 'F'),
 	blood_group CHAR(3) CHECK (blood_group IN ('A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-')),
-	
+	allergies ?,
+	phone_num char(11) UNIQUE NOT NULL,
+	email varchar(15) UNIQUE NOT NULL,
+	password varchar(8) NOT NULL,
+	role char(7) CHECK(role IN ('ADMIN', 'PATIENT')),
+	CONSTRAINT chk_password_length CHECK(CHAR_LENGTH(password) >= 8)
+);	
+
+CREATE TABLE appointment(
+	appointment_id INT PRIMARY KEY AUTO_INCREMENT,
+	patient_id INT NOT NULL,
+	schedule_id INT NOT NULL,
+	status varchar(10) CHECK(status IN('ACTIVE', 'ABSENT', 'DONE'),
+	CONSTRAINT fk_appTpat FOREIGN KEY patient_id REFERENCES patient(patient_id)
+	ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT fk_appTsch FOREIGN KEY schedule_id REFERENCES doctor_schedule(schedule_id)
+	ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE treatment(
+	treatment_id INT PRIMARY KEY AUTO_INCREMENT,
+	appointment_id INT NOT NULL,
+	icd_code varchar(10) NOT NULL,
+	CONSTRAINT fk_treTapp FOREIGN KEY appointment_id REFERENCES appointment(appointment_id)
+	ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE bill(
+	bill_id INT PRIMARY KEY AUTO_INCREMENT,
+	treatment_id INT NOT NULL,
+	cost DECIMAL(10, 2),
+	CONSTRAINT fk_bilTtre FOREIGN KEY treatment_id REFERENCES treatment(treatment_id)
+	ON DELETE CASCADE ON UPDATE CASCADE
+);
