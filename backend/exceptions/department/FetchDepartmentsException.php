3a740019c6f40d3c3e7973f120c8d12b44a01b43
YILMAZ SÖNMEZ <ysonmez824@gmail.com>
2026-04-30 19:45:26 +0300

    departments replaced

--- backend/exceptions/department/FetchDepartmentsException.php
+++
@@ -1,9 +0,0 @@
-<?php
-class FetchDepartmentsException extends Exception {
-    private const message = "Error while getting department data.";
-    private const int status = 500; // HTTP Status Code
-    function __construct(string $message = self::message, int $code = self::status){
-        return parent::__construct($message, $code);
-    }
-}
-?>
--- backend/exceptions/gender/FetchGenderException.php
+++
@@ -1,9 +0,0 @@
-<?php
-    class FetchGenderException extends Exception{
-        private const message = "Error while getting gender data.";
-        private const int status = 500; // HTTP Status Code
-        function __construct(string $message = self::message, int $code = self::status){
-            return parent::__construct($message, $code);
-        }
-    }
-?>
