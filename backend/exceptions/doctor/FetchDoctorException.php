0bb71b79f0e2136f45d12baa980af02baa7f06d5
ErzakFaresi <erencanerarslan16@gmail.com>
2026-05-06 15:26:56 +0300

    Erencan's first commit

---
+++ backend/exceptions/doctor/FetchDoctorException.php
@@ -0,0 +1,11 @@
+<?php
+
+class FetchDoctorException extends Exception {
+    private const message = "Error while getting doctor data.";
+    private const int status = 500; // HTTP Status Code
+    function __construct(string $message = self::message, int $code = self::status){
+        return parent::__construct($message, $code);
+    }
+}
+
+?>
