f578ad5cffeab1d5635d12945d999c9eea48b4ac
YILMAZ SÖNMEZ <ysonmez824@gmail.com>
2026-05-11 16:53:20 +0000

    adding exception handler
    creating resendCode logic
    
    fixing some bugs

--- backend/controllers/departmentController.php
+++ backend/controllers/departmentController.php
@@ -1,5 +1,4 @@
 <?php
-
 require_once '../dbConnect.php';
 // load all php files inside services/department
 foreach (glob("../services/department/*/*.php") as $filename) {
@@ -14,25 +13,20 @@ $action = $_GET['action'];
 
 $data = [];
 
-// catching all exceptions
-try{
-    switch($action){
-        case 'getDepartments':{
-            // if no exception, then
-            // turn dataset into json form;
-            $data = getDepartments($pdo);
-            echo responseEntity($data);
-        };
-        break;
+switch($action){
+    case 'getDepartments':{
+        // if no exception, then
+        // turn dataset into json form;
+        $data = getDepartments($pdo);
+        echo responseEntity($data);
+    };
+    break;
 
-        case 'filterDepartmentsByName':{
-            $data = filterDepartmentsByName($pdo);
-            echo responseEntity($data);
-        };
-        break;
-        default: echo responseEntity("Unknown Request", 400);
-        break;
+    case 'filterDepartmentsByName':{
+        $data = filterDepartmentsByName($pdo);
+        echo responseEntity($data);
+    };
+    break;
+    default: echo responseEntity("Unknown Request", 400);
+    break;
 }
-}catch(FetchDepartmentsException $e){ 
-    echo responseEntity($e->getMessage(), $e->getCode());
-} 
